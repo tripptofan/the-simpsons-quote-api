1bf296a1c0482611f36cb347dcd5d6f786673f4d
unknown <tripptofan@gmail.com>
2021-01-29 14:14:14 -0500

    fix ise

--- app.js
+++ app.js
@@ -10,21 +10,22 @@ const MongoClient = require('mongodb').MongoClient
 app.use(express.static(__dirname + '/public'))
 
 
-MongoClient.connect(process.env.DB_URL, { 
+MongoClient.connect(process.env.DATABASE_URL, { 
     useUnifiedTopology: true,
     useNewUrlParser: true
  },
    
  (err, database) => {
     //...start the server
-    if(err) return console.log(err)
+  
     app.get('/quotes', (req, res) => {
-        database.collection('quotes').aggregate([{ $project: {_id: 0, character: 1, quote: 1}}], (err, doc) => {
+        database.collection('quotes').aggregate([{ $project: {_id: 0, quote: 1, character: 1, characterDirection: 1}}], (err, doc) => {
             res.setHeader('Content-Type', 'application/json')
             res.header("Access-Control-Allow-Origin", "*")
             res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type, Accept")
 
             res.send(doc)
+            console.log(err)
         })
 
 
