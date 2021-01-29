35805f9a8a6ca708e7598773ac4349866dfc406f
unknown <tripptofan@gmail.com>
2021-01-29 18:32:35 -0500

    working

--- app.js
+++ app.js
@@ -5,18 +5,9 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
 const Quotes = require('./models/quotes')
 
 app.use(express.static(__dirname + '/public'))
-// app.use(expressLayouts)
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-
-// app.set('view engine', 'ejs')
-// app.set('views', __dirname + '/views')
-// app.set('layout', 'layouts/layout')
-
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -27,12 +18,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to db'))
 
 
-
-
-// app.get('/', async (req, res) => {
-//     res.render('index')
-// })
-
 app.get('/quotes', async (req, res) => {
     try {
         const quotes = await Quotes.find({})
@@ -41,9 +26,5 @@ app.get('/quotes', async (req, res) => {
         console.error(message.err)
     }
 })
-
-
-
-
 
 app.listen(process.env.PORT || 3700)
--- models/quotes.js
+++ models/quotes.js
@@ -13,6 +13,10 @@ const quoteSchema = new mongoose.Schema({
     characterDirection: {
         type: String,
         required: true
+    },
+    image: {
+        type: String,
+        required: true
     }
 })
 
